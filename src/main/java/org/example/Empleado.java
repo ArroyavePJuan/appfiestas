8006009dfa03b33b0230c65dceb240a9e171ea27
arroyavepjuan <jarroyavepa@cesde.net>
2023-02-02 12:10:33 -0500

    se crean metodos en clase empleado

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class Empleado {
+
+    // Atributos
+    String nombre;
+
+    //Constructor
+
+    public Empleado() {
+
+    }
+
+
+    //Metodos especiales
+
+    //La licuadora
+    public String saludar(String nombres){
+        String saludo = "Hola " + nombres;
+        return saludo;
+    }
+    public int sumar(int numeroUno, int numeroDos){
+        return numeroUno + numeroDos;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,13 +7,23 @@ public class Main {
         //EN EL MAIN CREO LOS OBJETOS DE MIS CLASES
         //EN EL MAIN CREO UNA INSTANCIA DE MI CLASE
         //EN EL MAIN LE SACO COPIA A MIS CLASES Y CREO OBJETOS
-        Invitado objetoDeClaseInvitado=new Invitado();
-        Invitado objeto2=new Invitado(333,true,"Juan",10000);
+//        Invitado objetoDeClaseInvitado=new Invitado();
+//        Invitado objeto2=new Invitado(333,true,"Juan",10000);
+//
+//
+//        //obtener el valor de los atributos
+//        System.out.println(objetoDeClaseInvitado.nombre);
+//        System.out.println(objeto2.nombre);
 
+        Empleado objetoTipoEmpleado = new Empleado();
+        String saludo = objetoTipoEmpleado.saludar("Juan Jose");
+        int resultado = objetoTipoEmpleado.sumar(5, 3);
 
-        //obtener el valor de los atributos
-        System.out.println(objetoDeClaseInvitado.nombre);
-        System.out.println(objeto2.nombre);
-
+        //Accediendo a los metodods de mi clase empleado:
+        objetoTipoEmpleado.saludar("Juan Jose");
+        System.out.println(objetoTipoEmpleado.saludar("Juan Jose"));
+        System.out.println(saludo);
+        System.out.println(resultado);
     }
+
 }
